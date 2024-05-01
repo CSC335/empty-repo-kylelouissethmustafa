d5d2b5c256d171b566c500ee9792520acc5a5a12
Seth Jernigan <SethJernigan8@outlook.com>
2024-05-01 10:10:23 -0700

    Minor updates to tests

--- src/tests/MemoryGameTest.java
+++ src/tests/MemoryGameTest.java
@@ -72,119 +72,20 @@ class MemoryGameTest {
 		assertEquals(1, newGame2.getTheme());
 	}
 	
-//	@Test
-//	void testBadGuess() {
-//		MemoryGame game = new MemoryGame(0, 2, 0);
-//		game.initGame();
-//		Card card1 = game.getBoard().getCard(0, 0);
-//		Card card2 = game.getBoard().getCard(0, 1);
-//		Card card3 = game.getBoard().getCard(1, 0);
-//		if(!game.checkMatch(card1, card2)) {
-//			game.cardClicked(0, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(0, 1, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//		} else if(!game.checkMatch(card1, card3)) {
-//			game.cardClicked(0, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(1, 0, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//		} else if(!game.checkMatch(card3, card2)) {
-//			game.cardClicked(1, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(0, 1, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//		}
-//		
-//		assertEquals(1, game.getNumMoves());
-//		assertEquals(10, game.getScore());
-//		assertEquals(0, game.getNumMatches());
-//		assertEquals(true, game.gameActive());
-//	}
-	
-//	@Test
-//	void testFlipBack() {
-//		MemoryGame game = new MemoryGame(0, 2, 0);
-//		game.initGame();
-//		Card card1 = game.getBoard().getCard(0, 0);
-//		Card card2 = game.getBoard().getCard(0, 1);
-//		Card card3 = game.getBoard().getCard(1, 0);
-//		if(!game.checkMatch(card1, card2)) {
-//			//game.cardClicked(0, 0, 1);
-//			//assertEquals(1, game.getNumCardsSelected());
-//			//game.cardClicked(0, 1, 1);
-//			//assertEquals(2, game.getNumCardsSelected());
-//			
-//			game.cardClicked(0, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(0, 1, 1);
-//			assertEquals(0, game.getNumCardsSelected());
-//		} else if(!game.checkMatch(card1, card3)) {
-//			//game.cardClicked(0, 0, 1);
-//			//assertEquals(1, game.getNumCardsSelected());
-//			//game.cardClicked(1, 0, 0);
-//			//assertEquals(2, game.getNumCardsSelected());
-//			
-//			game.cardClicked(0, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(1, 0, 1);
-//			assertEquals(0, game.getNumCardsSelected());
-//		} else if(!game.checkMatch(card3, card2)) {
-//			//game.cardClicked(1, 0, 1);
-//			//assertEquals(1, game.getNumCardsSelected());
-//			//game.cardClicked(0, 1, 1);
-//			//assertEquals(2, game.getNumCardsSelected());
-//			
-//			game.cardClicked(1, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(0, 1, 1);
-//			assertEquals(0, game.getNumCardsSelected());
-//		}
-//		
-//		assertEquals(1, game.getNumMoves());
-//		assertEquals(10, game.getScore());
-//		assertEquals(0, game.getNumMatches());
-//		assertEquals(true, game.gameActive());
-//	}
-	
-//	@Test
-//	void testNoThirdClick() {
-//		MemoryGame game = new MemoryGame(0, 2, 0);
-//		game.initGame();
-//		Card card1 = game.getBoard().getCard(0, 0);
-//		Card card2 = game.getBoard().getCard(0, 1);
-//		Card card3 = game.getBoard().getCard(1, 0);
-//		if(!game.checkMatch(card1, card2)) {
-//			game.cardClicked(0, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(0, 1, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//			
-//			game.cardClicked(1, 0, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//		} else if(!game.checkMatch(card1, card3)) {
-//			game.cardClicked(0, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(1, 0, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//			
-//			game.cardClicked(0, 1, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//		} else if(!game.checkMatch(card3, card2)) {
-//			game.cardClicked(1, 0, 1);
-//			assertEquals(1, game.getNumCardsSelected());
-//			game.cardClicked(0, 1, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//			
-//			game.cardClicked(0, 0, 1);
-//			assertEquals(2, game.getNumCardsSelected());
-//		}
-//		
-//		assertEquals(1, game.getNumMoves());
-//		assertEquals(10, game.getScore());
-//		assertEquals(0, game.getNumMatches());
-//		assertEquals(true, game.gameActive());
-//	}
+	@Test
+	public void testThreeMatch() {
+		Card card1 = new Card("1", "A");
+		Card card2 = new Card("1", "A");
+		Card card3 = new Card("1", "A");
+		Card card4 = new Card("1", "B");
+		
+		Accounts account = new Accounts("Mustafa", "Mustafa123");
+		MemoryGame game = new MemoryGame(0, 2, 0, account);
+		
+		assertTrue(game.checkMatch(card1, card2, card3));
+		assertFalse(game.checkMatch(card1, card2, card4));
+				
+	}
 	
 	@Test
 	void testProperTermination() {
